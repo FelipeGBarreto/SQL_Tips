2ebb7fa2c50b5ced02f9cc9060f60462ac845d55
Felipe Barreto <75122112+FelipeGBarreto@users.noreply.github.com>
2023-06-07 07:38:56 -0300

    function adjusted

--- lastest_months.sql
+++ lastest_months.sql
@@ -3,7 +3,7 @@ t_calendar as (
 select 
   calendar_date
   ,regexp_replace(substring_index(calendar_date,'-',2), '-', '/') as year_month
-  ,regexp_replace(concat_ws('/',year(calendar_date),weekofyear(calendar_date)), '-', '/') as year_week
+  ,concat_ws('/',year(calendar_date),weekofyear(calendar_date)) as year_week
 from (
   select
     explode(
